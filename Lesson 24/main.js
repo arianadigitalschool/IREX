9507099ff2da22d6380693989369749f328d7ba3
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-01 10:15:19 +0100

    functions

---
+++ Lesson 24/main.js
@@ -0,0 +1,38 @@
+
+// function showMessage(){
+//     alert("This message is inside a function");
+// }
+
+// showMessage();
+
+// function sum(x,y){
+//     return x+y;
+// }
+
+// document.write(sum(20,5));
+
+
+// var arrowfunction = () => alert("Hello World");
+
+// arrowfunction();
+
+var arrowfunction = name => alert(`Hello ${name}`);
+
+arrowfunction("Ariana Rexhepi");
+
+
+function Firstname(){
+    var localVar = "Ariana";
+    alert(localVar);
+}
+
+
+Firstname();
+
+function x(minutes){
+    return minutes * 60;
+}
+
+x();
+
+document.write(x(60));
