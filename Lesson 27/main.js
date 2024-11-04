1461fefa9a6993a75b3531c6384c287637616fde
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-04 12:10:11 +0100

    conditionals

---
+++ Lesson 27/main.js
@@ -0,0 +1,30 @@
+var x = 10;
+var y =5;
+
+
+//10 > 5 -- yes
+
+
+if(x>y){
+    console.log("This is true");
+}else{
+    console.log("This is wrong");
+}
+
+if(x<y){
+    console.log("this is wrong");
+}else{
+    console.log("this is true");
+}
+
+if(y>x){
+    console.log("this is true");
+}else{
+    console.log("this is wrong");
+}
+
+if(x==7){
+    console.log("10 equals 7");
+}else{
+    console.log("10 does not equal 7");
+}
