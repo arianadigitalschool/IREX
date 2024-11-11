65fc941dc9e3c2a304a43baf80e19564fce109af
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-11 12:29:53 +0100

    reaction game

--- Lesson 32/main.js
+++ Lesson 32/main.js
@@ -8,22 +8,54 @@
 
 // printNames();
 
-var colors = ['red', 'green', 'blue', 'purple'];
+// var colors = ['red', 'green', 'blue', 'purple'];
 
-function changeColor(){
-    document.querySelector('body').style.background = 
-    colors[Math.floor(Math.random()*colors.length)];
+// function changeColor(){
+//     document.querySelector('body').style.background = 
+//     colors[Math.floor(Math.random()*colors.length)];
+// }
+
+// changeColor();
+
+// var names = ['Antigona', 'Grenti', 'Donika'];
+
+// function changeName(){
+//     document.querySelector('p').innerHTML = 
+//     names[Math.floor(Math.random()*names.length)];
+// }
+
+// setInterval(changeColor, 1000);
+// setInterval(changeName, 1000);
+
+var circle = document.getElementById("circle");
+var showTime = document.getElementById("time");
+var timeStart = new Date().getTime();
+
+function showCircle(){
+    circle.style.display = "block";
+    circle.style.top = Math.random() * 500 + "px"; 
+    circle.style.left = Math.random() * 900 + "px"; 
+    circle.style.backgroundColor = randomColor();
+    timeStart = new Date().getTime();
 }
 
-changeColor();
+showCircle();
 
-var names = ['Antigona', 'Grenti', 'Donika'];
-
-function changeName(){
-    document.querySelector('p').innerHTML = 
-    names[Math.floor(Math.random()*names.length)];
+circle.onclick = function(){
+    circle.style.display = "none";
+    setTimeout(showCircle, 1000);
+    var timeEnd = new Date().getTime();
+    var time = (timeEnd - timeStart)/1000;
+    showTime.innerHTML = time + 's';
 }
 
-setInterval(changeColor, 1000);
-setInterval(changeName, 1000);
+function randomColor(){
+    var s = '0123456789ABCDEF';
+    var color = "#";
 
+    for(i=0; i<6;i++){
+        color = color + s[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
