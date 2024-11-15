55fa1726417339190f977618a88da4616081ac79
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-15 10:48:59 +0100

    juqery and jquery ui

--- Lesson 34/main.js
+++ Lesson 34/main.js
@@ -1,12 +1,38 @@
 //jquery
-$('myElement').hide();
+// $('myElement').hide();
 
 //manipulate with HTML 
 // $('#myElement').text("New Text");
 
-$('.btn').click(function(){
-    console.log($('#myElement').text());
+// $('.btn').click(function(){
+//     console.log($('#myElement').text());
 
-    $('#myElement').text('Testing text');
-    $('#myElement').append(' <br> Extra text');
+//     $('#myElement').text('Testing text');
+//     $('#myElement').append(' <br> Extra text');
+// });
+
+// $('#btn3').click(function(){
+//     $('.hidden').show('slow');
+// });
+
+// $('#btn4').click(function(){
+//     $('.hidden').hide();
+// });
+
+// $('#square').click(function(){
+//     $('#square').animate({
+//         'width': '200px',
+//         'height': '200px'
+//     });
+    
+// });
+
+$('#sort').sortable();
+
+$(function(){
+    $('#accordion').accordion();
+});
+
+$(function(){
+    $('#tabs').tabs();
 });
