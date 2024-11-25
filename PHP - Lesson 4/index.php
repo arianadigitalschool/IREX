e96a322e6df525741dfa691ec8008535fa41af7b
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-25 12:19:05 +0100

    built in functions in php

---
+++ PHP - Lesson 4/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// phpinfo();
+
+// $x = "Hello";
+// print_r($x);
+
+
+$x = 5;
+echo gettype($x) . "<br>";
+
+$y = 10.5;
+echo gettype($y) . "<br>";
+
+$z = "Hello";
+echo gettype($z) . "<br>";
+
+
+function displayVersion(){
+    echo "this is php version".phpversion();
+    echo "\n";
+}
+
+displayVersion();
+?>
