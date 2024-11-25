915002fa15259df6fdaa5a660ef333b255b927ef
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-25 11:17:21 +0100

    lesson 2

---
+++ PHP - Lesson 2/index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$name = "Elton";
+echo "I am $name <br>";
+
+
+$x = 50;
+$y = 30;
+
+echo $x + $y . "<br>";
+echo $x - $y . "<br>";
+echo $x / $y . "<br>";
+echo $x * $y . "<br>";
+
+$a = "Ariana ";
+$b = "Rexhepi";
+
+$c = $a.$b. "<br>";
+
+echo "$c \n";
+
+$message = "Hello ";
+$message .= "World";
+
+echo $message."<br>";
+
+$the_string = "Ariana Rexhepi Test";
+
+// echo strlen($the_string);
+// echo str_word_count($the_string);
+// echo str_replace("Test", "Ma e mira", $the_string);
+echo strrev($the_string);
+
+
+
+
+?>
