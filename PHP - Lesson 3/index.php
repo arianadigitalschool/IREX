a9cadd1d01ca8183e2523a5a772b9b5b66aef9cd
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-25 12:10:35 +0100

    loops

--- PHP - Lesson 3/index.php
+++ PHP - Lesson 3/index.php
@@ -25,33 +25,64 @@
 //     echo "The value of number is positive";
 // }
 
-$day = "Test";
+// $day = "Test";
 
-switch($day){
-    case 'Monday':
-    echo "Today it is Monday";
-    break;
-    case 'Tuesday':
-    echo "Today it is Tuesday";
-    break;
-    case 'Wednesday':
-    echo "Today it is Wednesday";
-    break;
-    case 'Thursday':
-    echo "Today it is Thursday";
-    break;
-    case 'Friday':
-    echo "Today it is Friday";
-    break;
-    case 'Saturday':
-    echo "Today it is Saturday";
-    break;
-    case 'Sunday':
-    echo "Today it is Sunday";
-    break;
-    default:
-    echo "Invalid Day";
-    break;
+// switch($day){
+//     case 'Monday':
+//     echo "Today it is Monday";
+//     break;
+//     case 'Tuesday':
+//     echo "Today it is Tuesday";
+//     break;
+//     case 'Wednesday':
+//     echo "Today it is Wednesday";
+//     break;
+//     case 'Thursday':
+//     echo "Today it is Thursday";
+//     break;
+//     case 'Friday':
+//     echo "Today it is Friday";
+//     break;
+//     case 'Saturday':
+//     echo "Today it is Saturday";
+//     break;
+//     case 'Sunday':
+//     echo "Today it is Sunday";
+//     break;
+//     default:
+//     echo "Invalid Day";
+//     break;
+// }
+
+
+// $x = 1;
+
+// while($x<5){
+//     echo "The number is:  $x  <br>";
+//     $x++;
+// }
+
+// do{
+//     echo "The number is:  $x  <br>";
+//     $x++;
+// }while($x < 5);
+
+// for($x = 0; $x<=10; $x++){
+//     echo "The number is:  $x  <br>";
+// }
+
+$cars = array("BMW"=>"18", "AUDI"=>"20", "Tesla"=>"22");
+
+// foreach($cars as $car){
+//     echo "$car <br>";
+// }
+
+foreach($cars as $x => $value){
+    echo "$x => $value <br>";
 }
+
+
+
+
 
 ?>
