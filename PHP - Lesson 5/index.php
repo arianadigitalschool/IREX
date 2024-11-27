9125413c7c57bff927ca617b4051d0d4afb18609
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-11-27 02:49:01 -0800

    arrays

--- PHP - Lesson 5/index.php
+++ PHP - Lesson 5/index.php
@@ -42,4 +42,32 @@ echo "</tr>";
 } 
 
 echo "</table>";
+
+
+$arrays = array(
+    array(1,2,3),
+    array(1,2,3),
+    array(1,2,3)
+);
+
+for($i = 1; $i<4; $i++){
+    for($j = 1; $j < 4; $j++){
+        echo "Array $i, Elements : $j <br>";
+    }
+};
+
+
+$grade = array(
+    "Math" => "3",
+    "Art" => "4",
+    "History" => "5",
+    "Music" => "4",
+);
+
+// echo "Math Grade is " . $grade['Math'];
+
+foreach($grade as $subject => $grade){
+    echo "Subject: " . $subject . ", Grade : " . $grade;
+    echo "<br>";
+}
 ?>
