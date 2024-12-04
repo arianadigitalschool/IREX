25ef38089427385bb7b9be9c159105553fb71a4f
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-12-04 02:39:32 -0800

    GET, POST, Fetching data

---
+++ php - lesson 7/fetch.php
@@ -0,0 +1,10 @@
+<?php
+
+    include_once('config.php');
+
+    $sql = "SELECT * from users";
+    $getUsers = $connect->prepare($sql);
+    $getUsers->execute();
+
+    $users = $getUsers ->fetchAll();
+?>
