23d31231163485fca267c8ff7a9196f07fb74c84
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-12-06 14:34:57 +0100

    bug fixing

--- CRUD/edit.php
+++ CRUD/edit.php
@@ -41,7 +41,7 @@
         <input type="hidden" name="id" value="<?php echo $data['id']?>"><br>
         <input type="text" name="name" value="<?php echo $data['name']?>"><br>
         <input type="text" name="username" value="<?php echo $data['username']?>"><br>
-        <input type="email" name="email " value="<?php echo $data['email']?>"><br>
+        <input type="email" name="email" value="<?php echo $data['email']?>"><br>
         <button type="submit" name="update">Update</button>
     </form>  
 </body>
--- CRUD/index.php
+++ CRUD/index.php
@@ -41,6 +41,29 @@
 </body>
 </html> -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+    <form action="add.php" method="POST">
+       
+        <input type="text" name="name" placeholder="Name"><br>
+        <input type="text" name="username" placeholder="Username"><br>
+        <input type="email" name="email" placeholder="Email"><br><br>
+        <button type="submit" name="submit">Add</button>
+ 
+    </form>
+ 
+ 
+ 
+ 
+</body>
+</html>
+ 
 
 <!DOCTYPE html>
 <html>
