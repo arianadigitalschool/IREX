9d4abb6ea71de7a1bd447075596e57c4f4be632c
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2024-12-06 01:04:51 -0800

    delete - CRUD

--- CRUD/index.php
+++ CRUD/index.php
@@ -85,9 +85,8 @@
                     <td> <?= $user['name']?> </td>
                     <td> <?= $user['username']?> </td>
                     <td> <?= $user['email']?> </td>
+                    <td> <?= "<a href='delete.php?id=$user[id]'>Delete</a>" ?> </td>
                 </tr>
-            
-
         <?php
             }
         ?>
